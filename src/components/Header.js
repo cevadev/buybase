e4223c00ecd32125d434269c546b49343cb6b85a
cevadev <carlos.villanueva.altuna@gmail.com>
2023-03-08 21:03:12 -0300

    eComerce Project - BuyBase

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
 
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
 function Header() {
   return (
     <header>
@@ -18,12 +20,14 @@ function Header() {
         </div>
 
         {/*  Search input */}
-        <div>
-          <input type="text" />
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <MagnifyingGlassIcon className="h-12 p-4" />
         </div>
       </div>
-
-      {/** Bottom nav */}
       <div></div>
     </header>
   );
